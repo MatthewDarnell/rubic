4b3c769af66395abb9cb8aa3de4bb27ebbeb1f1e
Matthew Darnell <matthew.darnell@protonmail.com>
2023-08-09 09:25:01 -0400

    Add chopper crypto funcs and generating address

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate libc;
 
+use std::convert::TryInto;
+
 extern {
     //ECCRYPTO_STATUS SchnorrQ_FullKeyGeneration(unsigned char* SecretKey, unsigned char* PublicKey);
     //fn SchnorrQ_FullKeyGeneration(SecretKey: *mut u8, PublicKey: *mut u8) -> u8;
@@ -8,8 +10,21 @@ extern {
     //fn SchnorrQ_KeyGeneration(SecretKey: *const u8, PublicKey: *mut u8);
 
     //ECCRYPTO_STATUS CompressedKeyGeneration(unsigned char* SecretKey, unsigned char* PublicKey);
-    fn getPublicKey(privateKey: *const u8, publicKey: *mut u8) -> u8;
+
+    //bool getSubseed(const unsigned char* seed, unsigned char* subseed)
+    //void getPrivateKey(unsigned char* subseed, unsigned char* privateKey)
+    fn getSubseed(seed: *const u8, subseed: *mut u8) -> bool;
+    fn getPrivateKey(subseed: *const u8, privateKey: *mut u8);
+
+    fn getPublicKey(privateKey: *const u8, publicKey: *mut u8);
+
+    //void getIdentity(unsigned char* publicKey, char* identity, bool isLowerCase)
+    fn getIdentity(publicKey: *const u8, identity: *const u8, isLowerCase: bool);
+
     //fn CompressedKeyGeneration(SecretKey: *const u8, PublicKey: *mut u8) -> u8;
+
+    //void KangarooTwelve(unsigned char* input, unsigned int inputByteLen, unsigned char* output, unsigned int outputByteLen)
+    fn KangarooTwelve(input: *const u8, inputByteLen: u32, output: *mut u8, outputByteLen: u32);
 }
 
 fn string_to_seed_bytes(string: &str) -> Result<Vec<u8>, &'static str> {
@@ -43,8 +58,11 @@ fn subseed(string: &str, mut index: u32) -> Result<Vec<u8>, &'static str> {
                 }
                 index = index - 1;
             }
-            let hash1 = kangarootwelve_xkcp::hash(preimage.as_slice());
-            Ok(hash1.as_bytes().to_owned().to_vec())
+            let mut hash1: [u8; 32] = [0; 32];
+            unsafe {
+                KangarooTwelve(preimage.as_slice().as_ptr(), preimage.len() as u32, hash1.as_mut_ptr(), 32);
+            }
+            Ok(hash1.to_owned().to_vec())
         },
         Err(err) => Err(err)
     }
@@ -55,11 +73,16 @@ fn identity(seed: &str, index: u32) -> Result<Vec<u8>, &'static str> {
         Ok(hashed_vec) => {
             let generated_secret_key: [u8; 32] = hashed_vec.try_into().unwrap();
             let mut generated_public_key: [u8; 32] = [0; 32];
+            let mut generated_identity: [u8; 60] = [0; 60];
             println!("Generated Secret Key: {:?}", generated_secret_key);
             println!("Before Generating Public Key: {:?}", &generated_public_key);
             unsafe { getPublicKey(generated_secret_key.as_ptr(), generated_public_key.as_mut_ptr()) };
+            unsafe {
+                getIdentity(generated_public_key.as_ptr(), generated_identity.as_mut_ptr(), false);
+            }
             println!("Generated PubKey {:?}", generated_public_key);
-            Ok(generated_public_key.to_vec())
+
+            Ok(generated_identity.to_vec())
         },
         Err(err) => Err(err)
     }
@@ -76,10 +99,30 @@ fn main() {
     //println!("SchnorrQ_FullKeyGeneration Secret Key: (Len={:?}) {:?}", generated_secret_key.len(), generated_secret_key);
     //println!("SchnorrQ_FullKeyGeneration Public Key: (Len={:?}) {:?}", generated_public_key.len(), generated_public_key);
 
-    let test_seed = "lcehvbvddggkjfnokduyjuiyvkklrvrmsaozwbvjlzvgvfipqpnkkuf";
+/*
     let val = identity(test_seed, 0).unwrap();
+    let id = std::str::from_utf8(val.as_slice()).unwrap();
     println!("Seed=({})", &test_seed);
-    println!("identity returned (Len={}) - {:?}", val.len(), val);
+    println!("identity returned (Len={}) - {:?}", val.len(), id);
+*/
 
+    let test_seed = "lcehvbvddggkjfnokduyjuiyvkklrvrmsaozwbvjlzvgvfipqpnkkuf";
+
+    let mut ownSubseed: [u8; 32] = [0; 32];
+    let mut privateKey: [u8; 32] = [0; 32];
+    let mut publicKey: [u8; 32] = [0; 32];
+    let mut identity: [u8; 60] = [0; 60];
+
+    let seed_bytes: Vec<u8> = test_seed.try_into().unwrap();
+    println!("Seed {:?}", test_seed);
+    println!("Seed Bytes: {:?}", &seed_bytes);
+    unsafe {
+        getSubseed(seed_bytes.as_slice().as_ptr(), ownSubseed.as_mut_ptr());
+        getPrivateKey(ownSubseed.as_ptr(), privateKey.as_mut_ptr());
+        getPublicKey(privateKey.as_ptr(), publicKey.as_mut_ptr());
+        getIdentity(publicKey.as_ptr(), identity.as_mut_ptr(), false);
+    }
+    let id = std::str::from_utf8(identity.as_slice()).unwrap();
+    println!("identity returned (Len={}) - {:?}", id.len(), id);
 
 }
