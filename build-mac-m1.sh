da6cd5a2b25ae3cb1fe6ce5da7b1d8fc7feb0ae7
Matthew Darnell <matthew.darnell@protonmail.com>
2023-12-04 13:06:09 -0500

    Make sure Mac M1 is using correct rust toolchain

--- build-mac-m1.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-rustup install nightly;
-grep -r -l 'malloc.h' ffi-deps/FourQlib/FourQ_32bit/schnorrq.c | sort | uniq | xargs perl -e "s/malloc.h/stdlib.h/" -pi
-cargo +nightly build;
---
+++ run-mac-m1.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+rustup default stable-aarch64-apple-darwin;
+rustup install nightly-aarch64-apple-darwin;
+rustup default nightly-aarch64-apple-darwin;
+grep -r -l 'malloc.h' ffi-deps/FourQlib/FourQ_32bit/schnorrq.c | sort | uniq | xargs perl -e "s/malloc.h/stdlib.h/" -pi
+cargo run;
