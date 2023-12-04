896b4711e09a43095f2706dd91a862cd61c6eea1
Matthew Darnell <matthew.darnell@protonmail.com>
2023-12-03 22:24:04 -0500

    Change missing malloc.h include to stdlib in fourq

--- build-mac-m1.sh
+++ build-mac-m1.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-cargo +nightly build
+grep -r -l 'malloc.h' ffi-deps/FourQlib/FourQ_32bit/schnorrq.c | sort | uniq | xargs perl -e "s/malloc.h/stdlib.h/" -pi
+cargo +nightly build;
