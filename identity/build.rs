4f9d8a64563086adc5e8d889ee3e7ad624e997c5
Matthew Darnell <matthew.darnell@protonmail.com>
2023-12-09 19:47:29 -0500

    Building on Mac M1

--- identity/build.rs
+++ identity/build.rs
@@ -10,7 +10,7 @@ fn main() {
             .define("_AVX_", "1")
             .include("../ffi-deps/FourQlib/FourQ_32bit/FourQ.h")
             .compile("Chopper")
-    } else {
+    } else if std::env::consts::OS == "linux" {
         cc::Build::new()
             .define("__LINUX__", "1")
             .define("_X86_", "1")
@@ -37,6 +37,32 @@ fn main() {
             .define("_AVX_", "1")
             .include("../ffi-deps/FourQlib/FourQ_32bit")
             .file("../ffi-deps/FourQlib/FourQ_32bit/FourQ.h")
+            .compile("Chopper")
+    } else {
+        cc::Build::new()
+            .define("__LINUX__", "1")
+            .define("_AMD64_", "1")
+            .define("_ARM_", "1")
+            .define("_AVX_", "1")
+            .define("USE_ENDO", "true")
+            .include("../ffi-deps/FourQlib/FourQ_32bit")
+            .file("../ffi-deps/FourQlib/FourQ_32bit/eccp2.c")
+            .file("../ffi-deps/FourQlib/FourQ_32bit/eccp2_no_endo.c")
+            .file("../ffi-deps/FourQlib/FourQ_32bit/crypto_util.c")
+            .file("../ffi-deps/FourQlib/FourQ_32bit/schnorrq.c")
+            .file("../ffi-deps/FourQlib/FourQ_32bit/kex.c")
+            .file("../ffi-deps/FourQlib/random/random.c")
+            .file("../ffi-deps/FourQlib/sha512/sha512.c")
+            .compile("libFourQ");
+
+
+        println!("cargo:rustc-link-lib=libFourQ");
+        println!("cargo:rustc-link-lib=dylib=libFourQ");
+
+        cc::Build::new()
+            .file("../ffi-deps/chopper-linux.cpp")
+            .define("__LINUX__", "1")
+            .define("_AMD64_", "1")
             .compile("Chopper")
     }
 }
