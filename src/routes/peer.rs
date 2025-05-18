6a3c7132f251a6f545a0611f743e5149fc3e9e30
Matthew Darnell <matthew.darnell@protonmail.com>
2025-05-18 16:25:25 -0400

    Cleanup addpeer

--- src/routes/peer.rs
+++ src/routes/peer.rs
@@ -1,13 +1,6 @@
-use std::collections::HashMap;
 use std::net::Ipv4Addr;
 use std::str::FromStr;
-use std::sync::mpsc::Sender;
-use std::sync::Mutex;
-use std::time::Duration;
 use rocket::get;
-use spmc::Receiver;
-use uuid::Uuid;
-use logger::error;
 use network::peer::Peer;
 use store;
 
@@ -26,8 +19,7 @@ pub fn peers() -> String {
 pub fn add_peer(address: &str) -> String {
     match Ipv4Addr::from_str(address) {
         Ok(_) => {
-            let new_peer = Peer::new(address, None, "");
-            let id = new_peer.get_id().to_owned();
+            Peer::new(address, None, "");
             match store::sqlite::crud::peer::fetch_peer_by_ip(store::get_db_path().as_str(), address) {
                 Ok(_) => { "Peer Added".to_string() },
                 Err(_) => { "Failed To Add Peer".to_string() }
