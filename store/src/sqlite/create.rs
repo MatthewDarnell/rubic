986c25f9a41ec11d156fdfd7e931babd5e91b1b5
Matthew Darnell <matthew.darnell@protonmail.com>
2025-05-25 21:26:41 -0400

    Use SQLite WAL journal mode
    Update some deps

--- store/src/sqlite/create.rs
+++ store/src/sqlite/create.rs
@@ -2,6 +2,7 @@
 pub fn open_database(path: &str, create: bool) -> Result<sqlite::Connection, String> {
     let query = "
     PRAGMA foreign_keys = ON;
+    PRAGMA journal_mode = WAL;
     CREATE TABLE IF NOT EXISTS peer (
       id TEXT UNIQUE NOT NULL PRIMARY KEY,
       ip TEXT UNIQUE NOT NULL,
